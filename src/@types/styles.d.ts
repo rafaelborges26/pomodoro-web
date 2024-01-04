7808eb49d7a79fe0c3468446cc29be136a83b9b6
Rafael Borges <rafael.borges2698@gmail.com>
2024-01-04 01:20:25 -0300

    first commit with themes

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components' //importando para conseguir somente adicionar mais tipagens
+import { defaultTheme } from '../styles/themes/default'
+
+type ThemeType = typeof defaultTheme //pegando a tipagem do defaultThemes
+
+declare module 'styled-components' { //adicionando a tipagem na lib
+    export interface DefaultTheme extends ThemeType {}
+}

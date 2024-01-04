0b4dbd490185043a19ae58b4b322341c528d2f5e
Rafael Borges <rafael.borges2698@gmail.com>
2024-01-04 02:02:00 -0300

    added eslint on project

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,9 @@
-import 'styled-components' //importando para conseguir somente adicionar mais tipagens
+import 'styled-components' // importando para conseguir somente adicionar mais tipagens
 import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme //pegando a tipagem do defaultThemes
+type ThemeType = typeof defaultTheme // pegando a tipagem do defaultThemes
 
-declare module 'styled-components' { //adicionando a tipagem na lib
-    export interface DefaultTheme extends ThemeType {}
+declare module 'styled-components' {
+  // adicionando a tipagem na lib
+  export interface DefaultTheme extends ThemeType {}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <button>teste</button>
